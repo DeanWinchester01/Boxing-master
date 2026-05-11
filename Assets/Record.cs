7041fe49e3606e613bb3d0653d79ce0fbabe745a
Dean Winchester <59614870+DeanWinchester01@users.noreply.github.com>
2026-05-12 09:46:36 +1200

    added timestamp recorder

--- Assets/Record.cs
+++ Assets/Record.cs
@@ -1,16 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Record : MonoBehaviour
 {
-    
+    public AudioSource source;
+    public AudioClip clip;
+    bool record = false;
+    //List<float> timeStamps = new List<float>();
+    float time;
+
+
     void Start()
     {
-        
+        source.clip = clip;
+        record = true;
+    }
+
+    void OnRecordButton()
+    {
+        //timeStamps.Add(time);
+        print(time);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (record)
+        {
+            time += Time.deltaTime;
+        }
     }
 }
